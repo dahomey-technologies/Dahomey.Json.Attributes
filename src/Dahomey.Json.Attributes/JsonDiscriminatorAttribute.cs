32b1cbaafc992e4510336da847bed5875bbfc1ee
Michaël Catanzariti <catanzarii@yahoo.com>
2021-11-13 12:10:51 +0100

    Imported attributes assembly from Dahomey.Json

---
+++ src/Dahomey.Json.Attributes/JsonDiscriminatorAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Dahomey.Json.Attributes
+{
+    public enum DiscriminatorPolicy
+    {
+        Default,
+        Auto,
+        Never,
+        Always
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class JsonDiscriminatorAttribute : Attribute
+    {
+        public object Discriminator { get; set; }
+
+        public DiscriminatorPolicy Policy { get; set; }
+
+        public JsonDiscriminatorAttribute(object discriminator)
+        {
+            Discriminator = discriminator;
+        }
+    }
+}
---
+++ src/Dahomey.Json.Attributes/JsonPropertyAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+[assembly: CLSCompliant(true)]
+namespace Dahomey.Json.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class JsonPropertyAttribute : Attribute
+    {
+    }
+}
